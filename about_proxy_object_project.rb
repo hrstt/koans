bd0256d04f7af09c084b7c48aa147b5a0518f2a9
sato hiroyuki <hiro.sato.tea@gmail.com>
2011-07-16 11:30:50 +0900

    damaged my karma about proxy object project

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,31 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_accessor :messages
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = Hash.new 0
   end
 
   # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    @messages[method_name] += 1
+    @object.send(method_name, *args, &block)
+  end
+  
+  def messages
+    @messages.keys
+  end
+  
+  def called?(method_name)
+    @messages.key?(method_name)
+  end
+  
+  def number_of_times_called(method_name)
+    @messages[method_name]
+  end
+  
 end
 
 # The proxy object should pass the following Koan:
